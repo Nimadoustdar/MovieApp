ded50a7a67ddf4c51cd229c703914127add7966c
NimaDoustdar <NimaDoustdar99@gamil.com>
2022-08-13 17:01:28 +0430

    initial commit

---
+++ src/Pages/Series/Searies.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Searies = () => {
+    return (
+        <div>
+            <span className='pageTitle'>
+                Searies
+            </span>
+        </div>
+    )
+}
+
+export default Searies
